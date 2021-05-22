71e8d29da132fb75b4e80fae53495f24a09cf42e
DavydKaio <davyd.kaio@gmail.com>
2021-05-22 20:00:11 -0300

    Aceitando conexões de clientes

--- server/server.go
+++ server/server.go
@@ -2,6 +2,8 @@ package main
 
 import (
 	"fmt"
+	"net"
+	"os"
 )
 
 const (
@@ -12,4 +14,22 @@ const (
 
 func main() {
 	fmt.Println("Starting " + connType + " server on " + connHost + ":" + connPort)
+	ln, err := net.Listen(connType, connHost+":"+connPort)
+	if err != nil {
+		fmt.Println("Error Listening: " + err.Error())
+		os.Exit(1)
+	}
+	defer ln.Close()
+
+	for {
+		conn, err := ln.Accept()
+		if err != nil {
+			fmt.Println("Error Conecting: " + err.Error())
+			return
+		}
+
+		fmt.Println("Client Connected")
+
+		fmt.Println("Client " + conn.RemoteAddr().String() + " connected.")
+	}
 }
