e82bd617f0d575eb5908c5a0e6310c33bbeb6355
DavydKaio <davyd.kaio@gmail.com>
2021-05-22 19:44:29 -0300

    Organizando arquivo servidor

--- server/server.go
+++ server/server.go
@@ -1,1 +1,15 @@
 package main
+
+import (
+	"fmt"
+)
+
+const (
+	connHost = "localhost"
+	connPort = "8080"
+	connType = "tcp"
+)
+
+func main() {
+	fmt.Println("Starting " + connType + " server on " + connHost + ":" + connPort)
+}
