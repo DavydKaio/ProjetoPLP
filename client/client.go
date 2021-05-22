d5f4a4799d4c2e1173ed85a8e0eeeac0e495cd28
DavydKaio <davyd.kaio@gmail.com>
2021-05-22 20:59:56 -0300

    Cliente finalizado

--- client/client.go
+++ client/client.go
@@ -1,7 +1,11 @@
 package main
 
 import (
+	"bufio"
 	"fmt"
+	"log"
+	"net"
+	"os"
 )
 
 const (
@@ -12,4 +16,24 @@ const (
 
 func main() {
 	fmt.Println("Connecting to " + connType + " server " + connHost + ":" + connPort)
+
+	conn, err := net.Dial(connType, connHost+":"+connPort)
+	if err != nil {
+		fmt.Println("Error Connecting: ", err.Error())
+		os.Exit(1)
+	}
+
+	reader := bufio.NewReader(os.Stdin)
+
+	for {
+		fmt.Print("Text to send: ")
+
+		input, _ := reader.ReadString('\n')
+
+		conn.Write([]byte(input))
+
+		message, _ := bufio.NewReader(conn).ReadString('\n')
+
+		log.Print("Server relay: " + message)
+	}
 }
