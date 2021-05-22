204f80e2266faeb576fb7b216b57ef5bd25fff59
DavydKaio <davyd.kaio@gmail.com>
2021-05-22 20:41:42 -0300

    Primeira configuração do cliente

--- client/client.go
+++ client/client.go
@@ -1,1 +1,15 @@
 package main
+
+import (
+	"fmt"
+)
+
+const (
+	connHost = "localhost"
+	connPort = "8080"
+	connType = "tcp"
+)
+
+func main() {
+	fmt.Println("Connecting to " + connType + " server " + connHost + ":" + connPort)
+}
